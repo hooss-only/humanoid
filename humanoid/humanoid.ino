c05c24dcb8b95b7b3087cab4d4ca916c0b338398
hooss-only <hoossonly@gmail.com>
2024-05-24 11:57:53 +0900

    feat: eyes finish

--- humanoid/humanoid.ino
+++ humanoid/humanoid.ino
@@ -4,8 +4,8 @@
 Servo eyesVerticalServo;
 Servo eyesHorizontalServo;
 Servo headSpinServo;
-int eyesVertical = 0;
-int eyesHorizontal = 130; // 90 ~ 180
+int eyesVertical = 120;
+int eyesHorizontal = 150; // 90 ~ 180
 const int EYES_SENSITIVE = 10;
 
 const long EYES_VERTICAL_PIN = 4;
@@ -13,8 +13,11 @@ const long EYES_HORIZONTAL_PIN = 5;
 const long RECV_PIN = 7;
 const long HEAD_SPIN_PIN = 8;
 
+const int EYES_VERTICAL_MINIMUM = 60;
+const int EYES_VERTICAL_MAXIMUM = 160;
+
 const int EYES_HORIZONTAL_MINIMUM = 110;
-const int EYES_HORIZONTAL_MAXIMUM = 180;
+const int EYES_HORIZONTAL_MAXIMUM = 190;
 
 IRrecv irrecv(RECV_PIN);
 
@@ -24,6 +27,7 @@ void setup() {
 	headSpinServo.attach(HEAD_SPIN_PIN);
 	Serial.begin(9600);
 	IrReceiver.begin(RECV_PIN, ENABLE_LED_FEEDBACK);
+	moveEyes();
 }
 
 bool eyesMode = false;
@@ -42,20 +46,22 @@ void RemoteControlHandler() {
 			Serial.println("Eyes mode enabled");
 		} else if (eyesMode) {
 			if (irNum == "46") {
-				eyesVertical += EYES_SENSITIVE;
+				eyesVertical -= EYES_SENSITIVE;
 			} else if (irNum == "44") {
 				eyesHorizontal += EYES_SENSITIVE;
 			} else if (irNum == "43") {
 				eyesHorizontal -= EYES_SENSITIVE;
 			} else if (irNum == "15") {
-				eyesVertical -= EYES_SENSITIVE;
+				eyesVertical += EYES_SENSITIVE;
 			} else if (irNum == "16") {
 				eyesMode = false;
 				Serial.println("Eyes mode disabled");
 			}
 
-			if (eyesVertical < 0) {
-				eyesVertical = 0;
+			if (eyesVertical < EYES_VERTICAL_MINIMUM) {
+				eyesVertical = EYES_VERTICAL_MINIMUM;
+			} else if (eyesVertical > EYES_VERTICAL_MAXIMUM) {
+				eyesVertical = EYES_VERTICAL_MAXIMUM;
 			}
 			
 			if (eyesHorizontal < EYES_HORIZONTAL_MINIMUM) {
@@ -65,6 +71,7 @@ void RemoteControlHandler() {
 			}
 
 			Serial.println(eyesHorizontal);
+			Serial.println(eyesVertical);
 			moveEyes();
 		}
 
